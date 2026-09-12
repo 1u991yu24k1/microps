fc89eb554c967ec24187327a1253a1d0348cb1d3
rmyc <mycd9427@gmail.com>
2026-09-13 02:38:04 +0900

    1.6(@P58)まで実装完了

--- net.c
+++ net.c
@@ -1,10 +1,105 @@
 /*
   プロトコルスタックのメインコード.
+*/
 
- */
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <sys/types.h>
 #include "platform.h"
-
 #include "util.h"
+#include "net.h"
+
+static struct net_device *devices; // Protocol Stackに登録しているネットワークデバイスのリスト
+
+// ネットワークデバイスのオブジェクト割当. 
+struct net_device *net_device_alloc(void) 
+{
+    struct net_device *dev;
+
+    dev = memory_alloc(sizeof(*dev));
+    if (!dev) {
+        errorf("memory_alloc()@%s", __func__);
+        goto out;
+    }
+out:
+    return dev;
+}
+
+
+// ネットワークデバイスを Protocol Stackに登録する. 
+// @dev: 新しく登録するネットワークデバイス. (caller側で非 NULL をチェックすること)
+// Returns:
+//  0: On Success
+// -1: On Failure
+int net_device_register(struct net_device *dev) 
+{
+    static unsigned int index = 0;
+    dev->index++;
+    snprintf(dev->name, sizeof(dev->name), "net%d", dev->index);
+    // 巡回リストにする. new_device -> old_device -> devices -> new_device -> ...
+    dev->next = devices;
+    devices = dev;
+    infof("success: dev=%s, type=0x%04x", dev->name, dev->type);
+    return 0;
+}
+
+// ネットワークデバイスを起動させる関数
+// @dev: 新しく登録するネットワークデバイス. (caller側で非 NULL をチェックすること)
+// Returns:
+//  0: On Success
+// -1: On Failure
+int net_device_open(struct net_device *dev)
+{
+    infof("dev=%s", dev->name);
+    if (NET_DEVICE_IS_UP(dev)) {
+        errorf("already opened, dev=%s", dev->name);
+        return -1;
+    }
+    // flag を up にセット
+    dev->flags |= NET_DEVICE_FLAG_UP;
+    return 0;
+}
+
+// ネットワークデバイスを停止させる関数
+// @dev: 新しく登録するネットワークデバイス. (caller側で非 NULL をチェックすること)
+// Returns:
+//  0: On Success
+// -1: On Failure
+int net_device_close(struct net_device *dev)
+{
+    infof("dev=%s", dev->name);
+    if (!NET_DEVICE_IS_UP(dev)) { // そもそも up していない.
+        errorf("not opened", dev->name);
+        return -1;
+    }
+    dev->flags &= ~NET_DEVICE_FLAG_UP;
+    return 0;
+}
+
+/* ネットワークデバイスからデータを送信する関数
+ *  Args:
+ *      @dev : 送信に使用するネットワークデバイス.
+ *      @type: 送信データのプロトコル種別. 
+ *      @data: 送信データのバイト列
+ *      @len : 送信データのバイト数
+ *      @dst : データリンク上の宛先アドレス (宛先 MAC アドレス) 
+    Returns:
+        0: 成功, -1: 失敗. 
+ */ 
+int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
+{
+    debugf("dev=%s, type=%04x, len=%zu", dev->name, dev->type, len);
+    debugdump(data, len);
+    if (!NET_DEVICE_IS_UP(dev)) { // opened なデバイスか?
+        errorf("not opened, dev=%s", dev->name);
+        return -1;
+    }
+    if (dev->mtu < len) {
+        
+    }
+    return 0;
+}
 
 /// プロトコルスタックの初期化
 int net_init(void)
@@ -21,10 +116,23 @@ int net_init(void)
 /// プロトコルスタックの起動
 int net_run(void)
 {
+    struct net_device *dev;
     infof("startup...");
     if (platform_init() == -1) {
         errorf("platform_init()@net_run failure");
         return -1;
+    }
+    /**
+     * ここでdevicesからすべての デバイスを open 状態にする.
+     * 書籍のコードでは, 
+     *  for (dev = devices; dev; dev = dev->next) { ... } となっているが, 
+     * net_device_registerの実装的に無限ループになる. そのため, 
+     *  for (dev = devices->next; dev != devices; dev = dev->next) { ... } に修正. 
+     */ 
+    for (dev = devices->next; dev != devices; dev = dev->next) {
+        if (net_device_open(dev) == -1) {
+            return -1;
+        }
     }
     infof(" net_run: success");
     return 0;
@@ -33,11 +141,25 @@ int net_run(void)
 /// プロトコルスタックの停止.
 int net_shutdown(void)
 {
+    struct net_device *dev = NULL; // 初期化しておく. 
     infof("shutting down...");
     if (platform_shutdown() == -1) {
         errorf("platform_shutdown()@net_shutdown failure");
         return -1;
     }
+    /**
+     * ここでdevicesからすべての デバイスを open 状態にする.
+     * 書籍のコードでは, 
+     *  for (dev = devices; dev; dev = dev->next) { ... } となっているが, 
+     * net_device_registerの実装的に無限ループになる.
+     */ 
+    for (dev = devices->next; dev != devices; dev = dev->next) {
+        if (net_device_close(dev) == -1) {
+            return -1;
+        }
+    }
+    devices = NULL;
     infof("net_shutdown: success");
     return 0;
 }
+
--- net.h
+++ net.h
@@ -1,8 +1,62 @@
-#ifndef NET_H
-#define NET_H
+#ifndef _NET_H
+#define _NET_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifndef IFNAMSIZ 
+#define IFNAMSIZ 16
+#endif /* IFNAMSIZ */
+
+// Device Types 
+#define NET_DEVICE_TYPE_DUMMY     0x0000
+#define NET_DEVICE_TYPE_LOOPBACK  0x0001
+#define NET_DEVICE_TYPE_ETHERNET  0x0002
+
+// Device status flags
+#define NET_DEVICE_FLAG_UP        0x0001
+#define NET_DEVICE_FLAG_LOOPBACK  0x0010
+#define NET_DEVICE_FLAG_BROADCAST 0x0020
+#define NET_DEVICE_FLAG_P2P       0x0040
+#define NET_DEVICE_NEED_ARP       0x0100
+
+#define NET_DEVICE_ADDR_LEN       16
+#define NET_DEVICE_IS_UP(x) ((x)->flags & NET_DEVICE_FLAG_UP)
+#define NET_DEVICE_STATE(x) (NET_DEVICE_IS_UP(x) ? "UP": "DOWN")
+
+/** 
+ * ネットワークデバイスを管理するための構造体. 
+ * 以下の情報を保持する.
+ *   デバイスの識別情報
+ *   ネットワークデバイスとデータリンクの特性
+ *   稼働状態
+ *   ハードウェアの物理アドレス.
+ * 
+ * Linuxカーネルでは, net_device構造体が相当する?
+ *  Ref: https://elixir.bootlin.com/linux/v7.2/source/include/linux/netdevice.h#L2149
+ */
+struct net_device {
+    struct net_device *next; // 片方向リンクリスト構造. 次デバイスへのポインタ
+    unsigned int index;      // 識別番号.
+    char name[IFNAMSIZ];     // デバイス名. 
+    uint16_t type;           // デバイスの種別. DUMMY: 0, LOOPBACK: 1, Ethernet: 2
+    uint16_t mtu;            // Maximum Transmission Unit
+    uint16_t flags;
+    uint16_t hlen;
+    uint16_t alen;
+    uint8_t  addr[NET_DEVICE_ADDR_LEN];
+    uint8_t  broadcast[NET_DEVICE_ADDR_LEN];
+};
+
+// Export 関数
+extern struct net_device *net_device_alloc(void);
+extern int net_device_register(struct net_device *dev);
+extern int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
 
 extern int net_init(void);
 extern int net_run(void);
 extern int net_shutdown(void);
 
-#endif
+#endif /* _NET_H */
+
+
